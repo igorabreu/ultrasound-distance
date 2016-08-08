da88cc26a86a9e724f551a9cdf1e5a10da05a6fe
Igor Abreu <igor.abreu.reis@gmail.com>
2016-08-07 22:47:09 -0300

    v1

---
+++ ultrasound-distance/ultrasound-distance.ino
@@ -0,0 +1,30 @@
+#define trigPin 11
+#define echoPin 10
+#define led 11
+#define led2 10
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+ 
+  if (distance >= 200 || distance <= 0){
+    Serial.write("out of range");
+  }
+  else {
+    Serial.write(distance);
+  }
+  delay(500);
+}
